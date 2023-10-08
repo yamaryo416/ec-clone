c3ea4b1814db874bd613cf98d8fbf89b312707c6
syunsuke-I <odol@ymail.ne.jp>
2023-10-08 11:53:31 +0900

    action mailの本番環境設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host:'https://railsecdemo-f5d108b8bb10.herokuapp.com/' }  
 end
